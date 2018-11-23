f7062898633ff1ba28a6a75fdab7a68c007fee4e
ben <wangabeng@163.com>
2018-11-23 13:25:23 +0800

    一个简单的sevelet demo

---
+++ project1/src/Ch1Servlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Ch1Servlet extends HttpServlet {
+  public void doGet (HttpServletRequest reque, HttpServletResponse response) throws IOException {
+    PrintWriter out = response.getWriter();
+    java.util.Date today = new java.util.Date();
+    out.println(
+        "<html>" +
+        "<body>" +
+        "<h1>this is my first project</h1>" +
+        "</body>" +
+        "</html>"
+    );
+  }
+}
